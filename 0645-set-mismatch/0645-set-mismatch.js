1023e84a5322c06f626af98e6daae3db98485d59
Prakash Das <144920398+kaku-coder@users.noreply.github.com>
2026-08-06 10:22:30 +0530

    Time: 13 ms (52.87%), Space: 63.7 MB (20.99%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.js
+++ 0645-set-mismatch/0645-set-mismatch.js
@@ -3,29 +3,48 @@
  * @return {number[]}
  */
 var findErrorNums = function (nums) {
-    let map = new Map()
+    // let map = new Map()
 
-    let duplicate
-    let missing
-    let arr = []
-    for (let i = 0; i < nums.length; i++) {
-        if (map.has(nums[i])) {
-            map.set(nums[i], map.get(nums[i]) + 1)
-        } else {
-            map.set(nums[i], 1)
-        }
+    // let duplicate
+    // let missing
+    // let arr = []
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (map.has(nums[i])) {
+    //         map.set(nums[i], map.get(nums[i]) + 1)
+    //     } else {
+    //         map.set(nums[i], 1)
+    //     }
+    // }
+    // for (let [key, value] of map) {
+    //     if (value > 1) {
+    //         duplicate = key
+    //     }
+    // }
+    // console.log(duplicate)
+    // for (let i = 1; i <= nums.length; i++) {
+    //     if (!map.has(i)) {
+    //         missing = i
+    //     }
+    // }
+    // arr.push(duplicate, missing)
+    // return arr
+    let set = new Set()
+let duplicate
+let missing
+let arr = []
+for (let i = 0; i < nums.length; i++) {
+    if(set.has(nums[i])){
+        duplicate= nums[i]
     }
-    for (let [key, value] of map) {
-        if (value > 1) {
-            duplicate = key
-        }
+    set.add(nums[i])
+}
+for (let i = 1; i <= nums.length; i++) {
+
+    if (!set.has(i)) {
+
+        missing = i
     }
-    console.log(duplicate)
-    for (let i = 1; i <= nums.length; i++) {
-        if (!map.has(i)) {
-            missing = i
-        }
-    }
-    arr.push(duplicate, missing)
+}
+arr.push(duplicate,missing)
     return arr
 };
