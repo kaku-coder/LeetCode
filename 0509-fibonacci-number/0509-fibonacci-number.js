7feeb6cd60e4a12ea27438957916c3af6af66e86
Prakash Das <144920398+kaku-coder@users.noreply.github.com>
2026-08-30 10:30:15 +0530

    Time: 40 ms (86.22%), Space: 54.1 MB (23.87%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n == 0) return 0
+    let first = 0;
+    let second = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let third = first + second;
+
+        first = second;
+        second = third;
+    }
+
+    return second;
+};
