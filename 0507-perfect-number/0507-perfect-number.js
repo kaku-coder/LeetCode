e709c52a1c7d93f418aba67a94f0fd1188965ee7
Prakash Das <144920398+kaku-coder@users.noreply.github.com>
2026-09-04 23:16:15 +0530

    Time: 1067 ms (35.83%), Space: 55.6 MB (15.96%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -1,17 +1,31 @@
-var checkPerfectNumber = function(num) {
+// var checkPerfectNumber = function(num) {
+//     if (num <= 1) return false;
+
+//     let sum = 1;
+
+ 
+//     // for (let i = 2; i * i <= num; i++) {
+//     //     if (num % i === 0) {
+//     //         sum += i; 
+//     //         if (i * i !== num) {
+//     //             sum += num / i; 
+//     //         }
+//     //     }
+//     // }
+
+//     // return sum === num;
+// };
+
+function checkPerfectNumber(num) {
     if (num <= 1) return false;
 
     let sum = 1;
 
-    // Loop sirf sqrt(num) tak chalega (i * i <= num)
-    for (let i = 2; i * i <= num; i++) {
+    for (let i = 2; i <= num / 2; i++) {
         if (num % i === 0) {
-            sum += i; // Chota factor
-            if (i * i !== num) {
-                sum += num / i; // Bada pair factor (jaise 28/2 = 14)
-            }
+            sum += i;
         }
     }
 
     return sum === num;
-};
+}
